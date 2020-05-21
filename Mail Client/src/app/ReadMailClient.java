12148515720af936c26d4dce215329e4d34113c6
Sandra Stojanović <sandramish96@gmail.com>
2020-05-22 01:26:36 +0200

    Zadatak1, DES algoritam

--- Mail Client/src/app/ReadMailClient.java
+++ Mail Client/src/app/ReadMailClient.java
@@ -1,24 +1,72 @@
 package app;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+
+import org.apache.xml.security.utils.JavaUtils;
 
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.model.Message;
 
 import support.MailHelper;
 import support.MailReader;
+import util.Base64;
+import util.GzipUtil;
 
 public class ReadMailClient extends MailClient {
 
+	private static final String KEY_FILE = "./data/session.key";
+
 	public static long PAGE_SIZE = 3;
 	public static boolean ONLY_FIRST_PAGE = true;
+	
+	//ucitavanje kljuca
+	private static SecretKey loadSecretKey(String fileName) {
+
+	    try {
+			//Od bajtova se kreira DES specifikacija
+	    	//bajtovi su od enkodiranog kljuca
+	    	DESKeySpec keySpec = new DESKeySpec(JavaUtils.getBytesFromFile(fileName));
+			//key factory transformise u objekat SecretKey
+	    	SecretKeyFactory skf = SecretKeyFactory.getInstance("DES");
+			SecretKey key = skf.generateSecret(keySpec);
+			     
+			return key;
+
+	    } catch (InvalidKeyException e) {
+			e.printStackTrace();
+			return null;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 	
 	public static void main(String[] args) throws IOException {
         // Build a new authorized API client service.
@@ -60,6 +108,53 @@ public class ReadMailClient extends MailClient {
 		MimeMessage chosenMessage = mimeMessages.get(answer);
 	    
         //TODO: Decrypt and decompress the message.
+	    
+	    try {
+			Cipher desCipherDec = Cipher.getInstance("DES/ECB/PKCS5Padding");
+			
+			SecretKey secretKey = loadSecretKey(KEY_FILE);
+			desCipherDec.init(Cipher.DECRYPT_MODE, secretKey);
+			
+			String text = MailHelper.getText(chosenMessage);
+			byte [] receivedText = Base64.decode(text);
+			
+			String subject = chosenMessage.getSubject();
+			byte [] receivedSubject = Base64.decode(subject);
+			
+			//dekriptovanje
+			String receivedTxt = new String(desCipherDec.doFinal(receivedText));
+			System.out.println("Primljeni text: " + new String(receivedTxt));
+			
+			String receivedSubject1 = new String(desCipherDec.doFinal(receivedSubject));
+			System.out.println("Primljeni subject: " + new String(receivedSubject1));
+		
+			
+			//decompress
+			String decompressedText = GzipUtil.decompress(Base64.decode(receivedTxt));
+			System.out.println("Primljeni text2: " + decompressedText);
+			
+			String decompressedSubject = GzipUtil.decompress(Base64.decode(receivedSubject1));
+			System.out.println("Primljeni subject 2 je: " + decompressedSubject);
+
+	    } catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	    
 	    
 	}
--- Mail Client/src/app/WriteMailClient.java
+++ Mail Client/src/app/WriteMailClient.java
@@ -1,42 +1,74 @@
 package app;
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.mail.internet.MimeMessage;
+
+import org.apache.xml.security.utils.JavaUtils;
 
 import com.google.api.services.gmail.Gmail;
 
 import support.MailHelper;
 import support.MailWritter;
+import util.GzipUtil;
+import util.Base64;
 
 public class WriteMailClient extends MailClient {
 
+	private static final String KEY_FILE = "./data/session.key";
+
 	public static void main(String[] args) {
-		
-        try {
-    		
-        	Gmail service = getGmailService();
-            
-        	System.out.println("Insert a reciever:");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String reciever = reader.readLine();
-        	
-            System.out.println("Insert a subject:");
-            String subject = reader.readLine();
-            
-            System.out.println("Insert text:");
-            String text = reader.readLine();
-            
-            
-            //TODO: Compress and encrypt the content before sending.     
-        	MimeMessage mimeMessage = MailHelper.createMimeMessage(reciever, subject, text);
-        	MailWritter.sendMessage(service, "me", mimeMessage);
-        	
-        }catch (Exception e) {
-        	e.printStackTrace();
+
+		try {
+
+			Gmail service = getGmailService();
+
+			System.out.println("Insert a reciever:");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			String reciever = reader.readLine();
+
+			System.out.println("Insert a subject:");
+			String subject = reader.readLine();
+
+			System.out.println("Insert text:");
+			String text = reader.readLine();
+
+			// TODO: Compress and encrypt the content before sending.
+
+			// compress
+			String compressedSubject = Base64.encodeToString(GzipUtil.compress(subject));
+			String compressedText = Base64.encodeToString(GzipUtil.compress(text));
+
+			// create key
+			// generator kljuceva za DES algoritam
+			KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+			// generise kljuc za DES
+			SecretKey secretKey = keyGen.generateKey();
+
+			// klasa za sifrovanje
+			Cipher desCipherEnc = Cipher.getInstance("DES/ECB/PKCS5Padding");
+			// inicijalizacija za sifrovanje
+			desCipherEnc.init(Cipher.ENCRYPT_MODE, secretKey);
+			// sifrovanje
+			byte[] cipherSubject = desCipherEnc.doFinal(compressedSubject.getBytes());
+			String cipherSubject1 = Base64.encodeToString(cipherSubject);
+
+			byte[] cipherText = desCipherEnc.doFinal(compressedText.getBytes());
+			String cipherText1 = Base64.encodeToString(cipherText);
+
+			// snimanje kljuca
+			JavaUtils.writeBytesToFilename(KEY_FILE, secretKey.getEncoded());
+
+			MimeMessage mimeMessage = MailHelper.createMimeMessage(reciever, cipherSubject1, cipherText1);
+			MailWritter.sendMessage(service, "me", mimeMessage);
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
+
 	}
 }
